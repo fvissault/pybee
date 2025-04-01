5eb4212f109d5cd156d654fa1be950b7476fe086
fvissault <fv35@yahoo.fr>
2025-04-01 13:30:34 +0200

    Update core.py

--- packages/core.py
+++ packages/core.py
@@ -192,7 +192,7 @@ class core(base_module):
         self.interpreter.compile['const'] = deque(['@'])
         self.interpreter.compile['2const'] = deque(['@'])
         self.help = core_help(self.interpreter.output)
-        self.version = 'v1.5.8'
+        self.version = 'v1.5.9'
 
     '''
     Instruction bye : quitte l'interpreteur Beetle
