c3656f8a1b05906fb0503ad3c0d13195b2fc61d7
fvissault <fv35@yahoo.fr>
2025-05-21 22:52:57 +0200

    Update web.py

--- packages/web.py
+++ packages/web.py
@@ -6,7 +6,7 @@ from packages.help.web_help import web_help
 class web(base_module):
     def __init__(self, interpreter):
         super().__init__(interpreter)
-        self.dictionary = {'generate' : '''dup "tag" cell@ local tag dup "content" cell@ local content dup "attrs" cell@ local attrs "container" cell@ local container 0 local item 0 local i 0 local k 0 local v tag @ "html" = if "<!DOCTYPE html>" .cr then "<<!0!>" [ tag @ ] format . attrs @ cells 0 > if attrs @ keys k ! attrs @ values v ! k @ cells i do " <!0!>='<!1!>'" [ k @ i @ cell@ v @ i @ cell@ ] format . loop then container @ "y" = if ">" .cr 0 i ! content @ cells i do content @ i @ cell@ item ! item @ ?str	if item @ .cr else item @ generate then	loop else "/>" .cr then	container @ "y" = if "</<!0!>>" [ tag @ ] format .cr then''',
+        self.dictionary = {'generate' : '''dup "tag" cell@ local tag dup "content" cell@ local content dup "attrs" cell@ local attrs "container" cell@ local container 0 local item 0 local i 0 local k 0 local v tag @ "html" = if "<!DOCTYPE html>" .cr then "<<!0!>" [ tag @ ] format . attrs @ cells 0 > if attrs @ keys k ! attrs @ values v ! k @ cells i do " <!0!>='<!1!>'" [ k @ i @ cell@ v @ i @ cell@ ] format . loop then container @ "y" = if ">" .cr 0 i ! content @ cells i do content @ i @ cell@ item ! item @ ?str if item @ .cr else item @ generate then loop else "/>" .cr then container @ "y" = if "</<!0!>>" [ tag @ ] format .cr then''',
                            'webreset' : '''"html" ?var if forget html then "head" ?var if forget head then "title" ?var if forget title then "body" ?var if forget body then "form" ?var if forget form then { tag : "html" , content : [ ] , attrs : { lang : "fr" } , container : "y" } var html { tag : "head" , content : [ ] , attrs : { } , container : "y" } var head { tag : "body" , content : [ ] , attrs : { } , container : "y" } var body html head addcontent html body addcontent { tag : "title" , content : [ ] , attrs : { } , container : "y" } var title head title addcontent { tag : "form" , content : [ ] , attrs : { action : "" , method : "post" } , container : "y" } var form cls''',
                            'addcontent' : '''dup ?var dup ?local or if @ then "content" reverse cell+''',
                            'addattr' : '''rot @ "attrs" cell@ cell+''',
@@ -41,7 +41,11 @@ class web(base_module):
                            'orderlist' : '''{ } local attrs "id" swap attrs cell+ drop { tag : "ol" , content : [ ] , attrs : attrs @ , container : "y" }''',
                            'listitem' : '''local cont { tag : "li" , content : [ cont @ ] , attrs : { } , container : "y" }''',
                            'urlbase' : '''{ } local attrs "target" rot attrs cell+ drop "href" rot attrs cell+ drop { tag : "base" , content : [ ] , attrs : attrs @ , container : "n" }''',
-                           'charset' : '''{ } local attrs "charset" rot attrs cell+ drop { tag : "meta" , content : [ ] , attrs : attrs @ , container : "n" }'''}
+                           'charset' : '''{ } local attrs "charset" rot attrs cell+ drop { tag : "meta" , content : [ ] , attrs : attrs @ , container : "n" }''',
+                           'httpequiv' : '''{ } local attrs "http-equiv" rot attrs cell+ drop "content" rot attrs cell+ drop { tag : "meta" , content : [ ] , attrs : attrs @ , container : "n" }''',
+                           'meta' : '''{ } local attrs "name" rot attrs cell+ drop "content" rot attrs cell+ drop { tag : "meta" , content : [ ] , attrs : attrs @ , container : "n" }'''
+                           }
         self.help = web_help(self.interpreter.output)
         self.version = 'v1.0.0'
+        self.variables = []
 
