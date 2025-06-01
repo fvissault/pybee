c760e9eb07ea8987d688091135f2c2aad640e43a
fvissault <fv35@yahoo.fr>
2025-06-01 14:11:20 +0200

    Update web_errors.py

--- packages/errors/web_errors.py
+++ packages/errors/web_errors.py
@@ -1,6 +1,7 @@
 from packages.errors.errors import error
 
 class web_errors:
-    error_fatal =                               error('fatal', 'fatal_message')
-    error_error =                               error('error', 'error_message')
-    error_warning =                             error('error', 'warning_message')
+    #error_fatal =                               error('fatal', 'fatal_message')
+    #error_error =                               error('error', 'error_message')
+    #error_warning =                             error('error', 'warning_message')
+    error_laystyle_not_found =                   error('error', 'layout style variable not found')
