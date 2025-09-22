b7c940b523e8e6640b7aa32001477e1c10e1ad9c
fvissault <fv35@yahoo.fr>
2025-09-22 15:53:08 +0200

    Update sidenav.js

--- userarea/js/sidenav.js
+++ userarea/js/sidenav.js
@@ -23,3 +23,15 @@ function closeNavOverlay() {
   document.getElementById("closebtn").style.display = "none";
   document.getElementById("sidenav").style.width = "40px";
 }
+
+function openLink(evt) {
+    var i, navlinks;
+
+    navlinks = document.getElementsByClassName("navlink");
+    for (i = 0; i < navlinks.length; i++) {
+        navlinks[i].className = navlinks[i].className.replace(" active", "");
+        navlinks[i].style.backgroundColor = "";
+    }
+
+    evt.currentTarget.className += " active";
+}
