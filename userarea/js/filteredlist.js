22db6607e48c85d549ba4a6e512f03e6e20cc7c0
fvissault <fv35@yahoo.fr>
2025-10-01 10:11:48 +0200

    Update filteredlist.js

--- userarea/js/filteredlist.js
+++ userarea/js/filteredlist.js
@@ -1,19 +1,35 @@
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
+/*******************************************************************************
+ * listfilter pour filtrer les éléments d'une liste
+ * Les éléments de la liste peuvent normalement être composés d'autres éléments
+ *******************************************************************************/
+function listfilter(listid, elt) {
+    var filter, ul, li, a, i, txtValue;
+    filter = elt.value.toUpperCase();
+    ul = document.getElementById(listid);
+    li = ul.getElementsByTagName('li');
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
+    for (i = 0; i < li.length; i++) {
+        txtValue = traverse(li[i]);
+        //txtValue = li[i].textContent || li[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
     }
-  }
+}
+
+function traverse(node) {
+    var text;
+    node.childNodes.forEach(child => {
+        if (child.nodeType === Node.ELEMENT_NODE) {
+            text = traverse(child);
+            return;
+        }
+        if (child.nodeType === Node.TEXT_NODE) {
+            text = child.nodeValue.trim();
+            return;
+        }
+    });
+    return text;
 }
