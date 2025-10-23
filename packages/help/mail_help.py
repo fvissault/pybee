d041aa77c47948e8f7855ab51cf3f4c287ee56f9
fvissault <fv35@yahoo.fr>
2025-10-23 23:51:21 +0200

    Update mail_help.py

--- packages/help/mail_help.py
+++ packages/help/mail_help.py
@@ -4,7 +4,7 @@ class mail_help(help):
     def __init__(self, output):
         super().__init__(output)
         self.help_dict = {
-            'sendmail' : '''Send a mail\nUsage : ( ... ) SENDMAIL ( ... )''',
-            'initmailer' : '''''',
-            'preparemail' : ''''''
+            'sendmail' : '''Send a mail to sendtomail\nUsage : ( "sendtomail" ... ) SENDMAIL ( ... )''',
+            'initmailer' : '''Initialize predefined mails and mail config variables\nUsage : ( ... ) INITMAILER ( ... )''',
+            'preparemail' : '''Preformats a multilingual email with dynamic parameters\nUsage : ( mailname maillang { dynamic_parameter1 , ... }... ) PREPAREMAIL ( { preparedmail } ... )'''
         }
