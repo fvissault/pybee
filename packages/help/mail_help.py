7ee108048687a128b8f26a4bee158e56c5c5f42e
fvissault <fv35@yahoo.fr>
2025-10-24 22:12:06 +0200

    Update mail_help.py

--- packages/help/mail_help.py
+++ packages/help/mail_help.py
@@ -6,5 +6,6 @@ class mail_help(help):
         self.help_dict = {
             'sendmail' : '''Send a mail to sendtomail\nUsage : ( "sendtomail" ... ) SENDMAIL ( ... )''',
             'initmailer' : '''Initialize predefined mails and mail config variables\nUsage : ( ... ) INITMAILER ( ... )''',
-            'preparemail' : '''Preformats a multilingual email with dynamic parameters\nUsage : ( mailname maillang { dynamic_parameter1 , ... }... ) PREPAREMAIL ( { preparedmail } ... )'''
+            'preparemail' : '''Preformats a multilingual email with dynamic parameters\nUsage : ( mailname maillang { dynamic_parameter1 , ... } ... ) PREPAREMAIL ( { preparedmail } ... )''',
+            'addattachment' : '''Add attachment to an email. You can attach more and more attachment to an email by using this word.\nUsage : ( "attachmentfile" ... ) ADDATTACHMENT ( { preparedmail } ... )'''
         }
