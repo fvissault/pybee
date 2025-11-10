ee6ad4a710434a31751fc6ba9eca88c2d91e7ddc
fvissault <fv35@yahoo.fr>
2025-11-10 08:56:34 +0100

    Update mysqldb_errors.py

--- packages/errors/mysqldb_errors.py
+++ packages/errors/mysqldb_errors.py
@@ -1,7 +1,7 @@
 from packages.errors.errors import error
 
 class mysqldb_errors:
-    error_name_expected =           error('error', 'name expected')
+    error_name_expected =           error('error', 'database name expected')
     error_connection_failed =       error('error', 'connection to database failed')
     error_action_type =             error('fatal', 'action type expected (database or table)')
     error_request_malformed =       error('fatal', 'request malformed')
