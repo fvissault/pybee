c3ff7ba88c32ed1a85164aa0db19da84462cdce8
fvissault <fv35@yahoo.fr>
2025-11-26 22:07:22 +0100

    Update io.py

--- packages/io.py
+++ packages/io.py
@@ -6,19 +6,17 @@ class Io:
     Instruction . : affiche et détruit le haut de la pile 
     '''
     def point_instr(self):
-        if len(self.work) > 0:
+        if self.require_stack(1, '?local') == None:
             temp = self.pop_work()
             temp = str(temp).replace('\\"', '"')
             print(temp, end='')
             return 'nobreak'
-        else:
-            return self.nothing_in_work('.')
 
     '''
     Instruction emit : affiche le caractère correpondant à son code
     '''
     def emit_instr(self):
-        if len(self.work) > 0:
+        if self.require_stack(1, 'emit') == None:
             temp = self.work[0]
             if isinstance(temp, int):
                 self.work.popleft()
@@ -26,8 +24,6 @@ class Io:
                 return 'nobreak'
             else:
                 return self.err('error_integer_expected', 'emit')
-        else:
-            return self.nothing_in_work('emit')
 
     '''
     Instruction cr : permet d'afficher un retour chariot dans la console
@@ -41,7 +37,7 @@ class Io:
     Instruction input : met en attente la console pour permettre à l'utilisateur de rentrer de l'information
     '''
     def input_instr(self):
-        if len(self.work) > 0:
+        if self.require_stack(1, 'input') == None:
             prompt = self.pop_work()
             ret = input(prompt)
             if self.isinteger(ret):
@@ -51,14 +47,12 @@ class Io:
             else:
                 self.work.appendleft(str(ret))
             return 'nobreak'
-        else:
-            return self.nothing_in_work('input')
 
     '''
     Instruction secinput : met en attente la console pour permettre à l'utilisateur de rentrer de l'information de manière sécurisée
     '''
     def secinput_instr(self):
-        if len(self.work) > 0:
+        if self.require_stack(1, 'secinput') == None:
             prompt = self.pop_work()
             ret = getpass.getpass(prompt)
             if self.isinteger(ret):
@@ -68,33 +62,27 @@ class Io:
             else:
                 self.work.appendleft(str(ret))
             return 'nobreak'
-        else:
-            return self.nothing_in_work('secinput')
 
     '''
     Instruction kpress : permet à l'uilisateur de controler le clavier
     '''
     def keypress_instr(self):
-        if len(self.work) > 0:
+        if self.require_stack(1, 'kpress') == None:
             key = self.pop_work()
             if keyboard.is_pressed(key):
                 self.work.appendleft(1)
             else:
                 self.work.appendleft(0)
             return 'nobreak'
-        else:
-            return self.nothing_in_work('kpress')
 
     '''
     Instruction readk : permet à l'uilisateur de controler le clavier
     '''
     def readkey_instr(self):
-        if len(self.work) > 0:
+        if self.require_stack(1, 'readk') == None:
             key = self.pop_work()
             if keyboard.read_key(suppress=True) == key:
                 self.work.appendleft(1)
             else:
                 self.work.appendleft(0)
             return 'nobreak'
-        else:
-            return self.nothing_in_work('readk')
