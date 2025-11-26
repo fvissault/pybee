3a84b164429cd1148d734054ca735bd5470b174a
fvissault <fv35@yahoo.fr>
2025-11-26 22:07:01 +0100

    Update logic.py

--- packages/logic.py
+++ packages/logic.py
@@ -4,7 +4,7 @@ class Logic:
     Instruction = : Egalité
     '''
     def equal_instr(self):
-        if len(self.work) > 1:
+        if self.require_stack(2, '=') == None:
             base = self.dictionary['base']
             op1 = self.pop_work()
             op2 = self.pop_work()
@@ -24,14 +24,12 @@ class Logic:
             else:
                 self.work.appendleft(0)
             return 'nobreak'   
-        else:
-            return self.nothing_in_work('=')
 
     '''
     Instruction > : Supérieur
     '''
     def sup_instr(self):
-        if len(self.work) > 1:
+        if self.require_stack(2, '>') == None:
             base = self.dictionary['base']
             op1 = self.pop_work()
             op2 = self.pop_work()
@@ -51,14 +49,12 @@ class Logic:
             else:
                 self.work.appendleft(0)
             return 'nobreak'   
-        else:
-            return self.nothing_in_work('>')
 
     '''
     Instruction < : Inférieur
     '''
     def inf_instr(self):
-        if len(self.work) > 1:
+        if self.require_stack(2, '<') == None:
             base = self.dictionary['base']
             op1 = self.pop_work()
             op2 = self.pop_work()
@@ -78,14 +74,12 @@ class Logic:
             else:
                 self.work.appendleft(0)
             return 'nobreak'   
-        else:
-            return self.nothing_in_work('<')
 
     '''
     Instruction >= : Supérieur ou égal
     '''
     def supequal_instr(self):
-        if len(self.work) > 1:
+        if self.require_stack(2, '>=') == None:
             base = self.dictionary['base']
             op1 = self.pop_work()
             op2 = self.pop_work()
@@ -105,14 +99,12 @@ class Logic:
             else:
                 self.work.appendleft(0)
             return 'nobreak'   
-        else:
-            return self.nothing_in_work('>=')
 
     '''
     Instruction <= : Inférieur ou égal
     '''
     def infequal_instr(self):
-        if len(self.work) > 1:
+        if self.require_stack(2, '<=') == None:
             base = self.dictionary['base']
             op1 = self.pop_work()
             op2 = self.pop_work()
@@ -132,44 +124,36 @@ class Logic:
             else:
                 self.work.appendleft(0)
             return 'nobreak'   
-        else:
-            return self.nothing_in_work('<=')
 
     '''
     Instruction and : et logique
     '''
     def and_instr(self):
-        if len(self.work) > 1:
+        if self.require_stack(2, 'and') == None:
             op1 = self.pop_work()
             op2 = self.pop_work()
             result = op1 and op2
             self.work.appendleft(result)
             return 'nobreak'
-        else:
-            return self.nothing_in_work('and')
 
     '''
     Instruction ou : ou logique
     '''
     def or_instr(self):
-        if len(self.work) > 1:
+        if self.require_stack(2, 'or') == None:
             op1 = self.pop_work()
             op2 = self.pop_work()
             result = op1 or op2
             self.work.appendleft(result)
             return 'nobreak'
-        else:
-            return self.nothing_in_work('or')
 
     '''
     Instruction xor : ou exclusif logique
     '''
     def xor_instr(self):
-        if len(self.work) > 1:
+        if self.require_stack(2, 'xor') == None:
             op1 = self.pop_work()
             op2 = self.pop_work()
             result = op1 ^ op2
             self.work.appendleft(result)
             return 'nobreak'
-        else:
-            return self.nothing_in_work('xor')
