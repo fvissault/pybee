9d59b26f4d0ff471cccd3a7348ec2b8b7a6780ca
fvissault <fv35@yahoo.fr>
2025-11-30 10:01:40 +0100

    Create db_help.py

---
+++ packages/help/db_help.py
@@ -0,0 +1,26 @@
+from packages.help.help import help
+
+class db_help(help):
+    def __init__(self, output):
+        super().__init__(output)
+        self.help_dict = {'?connect' : '',
+                          'connect' : '',
+                          'disconnect' : '',
+                          '?close' : '',
+                          'close' : '',
+                          'hostname' : '',
+                          'dbname' : '',
+                          'username' : '',
+                          'userpass' : '',
+                          'dbparam' : '',
+                          'use' : '',
+                          '|create>' : '',
+                          '|show>' : '',
+                          '|select>' : '',
+                          '|insert>' : '',
+                          '|update>' : '',
+                          '|delete>' : '',
+                          '|drop>' : '',
+                          '|truncate>' : '',
+                          '|alter>' : '',
+                          '>|' : ''}
