879a0d965bad31e46d89eb8bbd066ab6f600f538
fvissault <fv35@yahoo.fr>
2025-12-19 00:15:08 +0100

    Create mq.py

---
+++ packages/mq.py
@@ -0,0 +1,196 @@
+from collections import deque
+from packages.errors.core_errors import core_errors
+from packages.errors.mq_errors import mq_errors
+from packages.base_module import base_module
+from packages.help.mq_help import mq_help
+from math import *
+
+class mq(base_module):
+    def __init__(self, interpreter):
+        super().__init__(interpreter)
+        self.dictionary = {
+            #****************************************
+            'σx' : '''    ( matrice de Pauli sur x : ne comporte que des nombres complexes )
+    [ [ 0 0 complex 1 0 complex ]
+    [ 1 0 complex 0 0 complex ] ]''',
+            #****************************************
+            'σy' : '''    ( matrice de Pauli sur y : ne comporte que des nombres complexes )
+    [ [ 0 0 complex  0 -1 complex ]
+    [ 0 1 complex 0 0 complex ] ]''',
+            #****************************************
+            'σz' : '''    ( matrice de Pauli sur z : ne comporte que des nombres complexes )
+    [ [ 1 0 complex 0 0 complex ]
+    [ 0 0 complex -1 0 complex ] ]''',
+            #****************************************
+            'phi' : '''    966 stemit''',
+            #****************************************
+            'psi' : '''    968 stemit''',
+            #****************************************
+            'sigma' : '''    963 stemit''',
+            #****************************************
+            'alpha' : '''    945 stemit''',
+            #****************************************
+            'beta' : '''    946 stemit''',
+            #****************************************
+            'delta' : '''    948 stemit''',
+            #****************************************
+            'epsilon' : '''    949 stemit''',
+            #****************************************
+            'lambda' : '''    955 stemit''',
+            #****************************************
+            'mu' : '''    956 stemit''',
+            #****************************************
+            'omega' : '''    937 stemit''',
+            #****************************************
+            'nu' : '''    957 stemit''',
+            #****************************************
+            'tau' : '''    964 stemit''',
+            #****************************************
+            'omicron' : '''    959 stemit''',
+            #****************************************
+            'theta' : '''    952 stemit''',
+            #****************************************
+            'ket' : '''    ( |ψ> = [ [ a ] [ b ] [ c ] ] ou a, b, c sont des nombres complexes a = [ re(a) im(a) ], idem pour b et c )
+    local state
+    local content
+    content @ "const |<#0#>>" [ state @ ] format evaluate''',
+            #****************************************
+            'bra' : '''    ( <ψ| = [ a* b* c* ] ou a, b, c sont des nombres complexes a = [ re(a) im(a) ], idem pour b et c )
+    local state
+    local content
+    content @ "const <<#0#>|" [ state @ ] format evaluate''',
+            #****************************************
+            'braket' : '''    local brastate
+    local ketstate
+    local content
+    content @ "const <<#0#>|<#1#>>" [ brastate @ ketstate @ ] format evaluate''',
+            #****************************************
+            '>bra' : '''    ( transformation d'un ket en bra )
+    local ketname
+    "|<#0#>>" [ ketname @ ] format evaluate
+    madjoint
+    ketname @ bra''',
+            #****************************************
+            '>ket' : '''    ( transformation d'un bra en ket )
+    local braname
+    "<<#0#>|" [ braname @ ] format evaluate
+    madjoint
+    braname @ ket''',
+            #****************************************
+            'braket*' : '''    ( produit scalaire de bra et ket -> fournit un scalaire )
+    local k
+    local b
+    b @ k @ m*
+    0 cell@ matrix. 0 cell@''',
+            #****************************************
+            'ktensor' : '''    ( a b -- a ⊗ b )
+    local b
+    local a
+    [ ] local r
+    [ ] local rowa
+    [ ] local rowb
+    [ ] local rowr
+    0 local i
+    0 local j
+    0 local k
+    0 local l
+    0 local aleft
+    0 local bright
+    a @ cells i
+    do
+        0 k !
+        a @ i @ cell@ rowa !
+        b @ cells k
+        do
+            0 j !
+            b @ k @ cell@ rowb !
+            [ ] rowr !
+            rowa @ cells j
+            do
+                0 l !
+                rowb @ cells l
+                do
+                    rowa @ j @ cell@ aleft !
+                    rowb @ l @ cell@ bright !
+                    aleft @ ?array
+                    if
+                        aleft @ bright @ c*
+                    else
+                        aleft @ bright @ *
+                    then
+                    rowr cell+ drop 
+                loop
+            loop
+            rowr @ r cell+ drop
+        loop
+    loop
+    r @''',
+            #****************************************
+            '|0>' : '''    [ [ 1 0 complex ] [ 0 0 complex ] ]''',
+            #****************************************
+            '|1>' : '''    [ [ 0 0 complex ] [ 1 0 complex ] ]''',
+            #****************************************
+            '<0|' : '''    |0> madjoint''',
+            #****************************************
+            '<1|' : '''    |1> madjoint''',
+            #****************************************
+            '|+>' : '''    |0> |1> m+ 1 2 sqrt / mscalar*''',
+            #****************************************
+            '|->' : '''    |0> |1> m- 1 2 sqrt / mscalar*''',
+            #****************************************
+            '<+|' : '''    |+> madjoint''',
+            #****************************************
+            '<-|' : '''    |-> madjoint''',
+            #****************************************
+            'hgate' : '''    ( porte Hadamard )
+    [ [ 1 0 complex 1 0 complex ] [ 1 0 complex -1 0 complex ] ] 
+    1 2 sqrt / mscalar*''',
+            #****************************************
+            'igate' : '''    ( porte identité )
+    [ [ 1 0 complex 0 0 complex ] [ 0 0 complex 1 0 complex ] ]''',
+            #****************************************
+            'sgate' : '''    ( porte phase )
+    [ [ 1 0 complex 0 0 complex ] [ 0 0 complex 0 1 complex ] ]''',
+            #****************************************
+            'tgate' : '''    ( porte T (pi/8) )
+    [ [ 1 0 complex 0 0 complex ] [ 0 0 complex pi/4 cos pi/4 sin complex ] ]''',
+            #****************************************
+            'cnot' : '''    ( porte CNOT )
+    [ [ 1 0 complex 0 0 complex 0 0 complex 0 0 complex ] [ 0 0 complex 1 0 complex 0 0 complex 0 0 complex ] [ 0 0 complex 0 0 complex 0 0 complex 1 0 complex ] [ 0 0 complex 0 0 complex 1 0 complex 0 0 complex ] ]
+    ' |00> ?const invert 
+    if
+        |0> |0> ktensor "00" ket
+    then
+    ' |01> ?const invert 
+    if
+        |0> |1> ktensor "01" ket
+    then
+    ' |10> ?const invert 
+    if
+        |1> |0> ktensor "10" ket
+    then
+    ' |11> ?const invert 
+    if
+        |1> |1> ktensor "11" ket
+    then''',
+            #****************************************
+            'hmeasure' : '''    hgate |0> m* local state 
+    "État après Hadamard :" .cr 
+    state @ matrix. .cr 
+    0 local amp0 
+    0 local amp1 
+    <0| state @ m* amp0 ! 
+    <1| state @ m* amp1 ! 
+    0 local p0 
+    0 local p1 
+    amp0 @ 0 cell@ 0 cell@ norm 2 pow p0 ! 
+    amp1 @ 0 cell@ 0 cell@ norm 2 pow p1 ! 
+    "Probabilité de mesurer |0> :" .cr 
+    p0 @ f. .cr 
+    "Probabilité de mesurer |1> :" .cr 
+    p1 @ f. .cr'''
+        }
+        self.help = mq_help(interpreter.output)
+
+        self.version = 'v1.0.0'
+        self.packuse = ['math']
