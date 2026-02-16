dbf08e173eeb989ca0f492d6e4de67c233d8cafa
fvissault <fv35@yahoo.fr>
2026-02-16 18:02:31 +0100

    Update errors.py

--- packages/errors/errors.py
+++ packages/errors/errors.py
@@ -9,21 +9,39 @@ class error:
         if message != '':
             self.message = message
 
-    def print_error(self, prefix, output):
+    def print_error(self, prefix, interpreter):
         msg = prefix + ' ' + self.type + ' : ' + self.message
 
-        if output == 'console':
+        if interpreter.output == 'console':
             if self.type == 'warning':
                 print(termcolors.WARNING + msg + termcolors.NORMAL)
+                if interpreter.activelog:
+                    interpreter.core_instr.logwarn(msg)
             elif self.type == 'error':
                 print(termcolors.ERROR + msg + termcolors.NORMAL)
+                if interpreter.activelog:
+                    interpreter.core_instr.logerr(msg)
             elif self.type == 'fatal':
                 print(termcolors.FATAL + msg + termcolors.NORMAL)
+                if interpreter.activelog:
+                    interpreter.core_instr.logerr(msg)
             else:
                 print(msg)
+                if interpreter.activelog:
+                    interpreter.core_instr.loginfo(msg)
 
-        if output == 'web':
+        if interpreter.output == 'web':
             print("Content-type: text/html;charset=UTF-8\n")
             print(msg, end='<br>')
+            if interpreter.activelog:
+                if self.type == 'warning':
+                    interpreter.core_instr.logwarn(msg)
+                elif self.type == 'error':
+                    interpreter.core_instr.logerr(msg)
+                elif self.type == 'fatal':
+                    interpreter.core_instr.logerr(msg)
+                else:
+                    interpreter.core_instr.loginfo(msg)
+
 
         return 'break' if self.type == 'fatal' else 'nobreak'
