fa38ea3bc9662bcbd90017692c13840661997603
fvissault <fv35@yahoo.fr>
2026-02-17 14:09:41 +0100

    Update arithmetic.py

--- packages/arithmetic.py
+++ packages/arithmetic.py
@@ -1,35 +1,43 @@
+import traceback
+
 class Arithmetic:
 
     '''
     Instruction * : Fait la multiplication entre 2 nombres du haut de la pile de travail et ajoute le résultat en haut de la pile
     '''
     def prod_instr(self):
-        if self.require_stack(2, '*') == None:
-            base = self.dictionary['base']
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            if base == 10:
-                if isinstance(op1, str):
-                    op1 = float(op1)
-                if isinstance(op2, str):
-                    op2 = float(op2)
+        try:
+            if self.require_stack(2, '*') == None:
+                base = self.dictionary['base']
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                if base == 10:
+                    if isinstance(op1, str):
+                        op1 = float(op1)
+                    if isinstance(op2, str):
+                        op2 = float(op2)
+                    result = op1 * op2
+                    self.interpreter.work.appendleft(result)
+                    return 'nobreak'
+                else:
+                    if isinstance(op1, str):
+                        try:
+                            op1 = int(op1, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '*')
+                    if isinstance(op2, str):
+                        try:
+                            op2 = int(op2, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '*')
                 result = op1 * op2
-                self.interpreter.work.appendleft(result)
+                self.interpreter.work.appendleft(self.to_base(result, base))
                 return 'nobreak'
-            else:
-                if isinstance(op1, str):
-                    try:
-                        op1 = int(op1, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '*')
-                if isinstance(op2, str):
-                    try:
-                        op2 = int(op2, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '*')
-            result = op1 * op2
-            self.interpreter.work.appendleft(self.to_base(result, base))
-            return 'nobreak'
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     def to_base(self, number, base):
         base_string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -68,13 +76,19 @@ class Arithmetic:
         return self.err('error_invalid_litteral', '+')    
 
     def plus_instr(self):
-        if self.require_stack(2, '+') == None:
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            op3 = self.plus(op1, op2)
-            if op3 != "nobreak":
-                self.interpreter.work.appendleft(op3)
-            return 'nobreak'
+        try:
+            if self.require_stack(2, '+') == None:
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                op3 = self.plus(op1, op2)
+                if op3 != "nobreak":
+                    self.interpreter.work.appendleft(op3)
+                return 'nobreak'
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     def possiblebases(self, s: str, exclure10=True):
         bases = []
@@ -115,33 +129,45 @@ class Arithmetic:
 
 
     def minus_instr(self):
-        if self.require_stack(2, '-') == None:
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            op3 = self.minus(op2, op1)
-            self.interpreter.work.appendleft(op3)
-            return 'nobreak'
+        try:
+            if self.require_stack(2, '-') == None:
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                op3 = self.minus(op2, op1)
+                self.interpreter.work.appendleft(op3)
+                return 'nobreak'
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     '''
     Instruction / : Division entre 2 nombres du haut de la pile de travail et ajoute le résultat en haut de la pile
     La division dans une autre base que 10 n'est pas possible
     '''
     def div_instr(self):
-        if self.require_stack(2, '/') == None:
-            base = self.dictionary['base']
-            op1 = self.pop_work()
-            if op1 == 0:
-                return self.err('error_division_by_zero_invalid', '/')
-            op2 = self.pop_work()
-            if isinstance(op1, list) or isinstance(op2, list):
-                return self.err('error_invalid_litteral', '/')
-            if base == 10:
-                if isinstance(op1, str):
-                    op1 = float(op1)
-                if isinstance(op2, str):
-                    op2 = float(op2)
-            else:
-                return self.err('error_invalid_litteral', '/')
-            result = op2 / op1
-            self.interpreter.work.appendleft(result)
-            return 'nobreak'
+        try:
+            if self.require_stack(2, '/') == None:
+                base = self.dictionary['base']
+                op1 = self.pop_work()
+                if op1 == 0:
+                    return self.err('error_division_by_zero_invalid', '/')
+                op2 = self.pop_work()
+                if isinstance(op1, list) or isinstance(op2, list):
+                    return self.err('error_invalid_litteral', '/')
+                if base == 10:
+                    if isinstance(op1, str):
+                        op1 = float(op1)
+                    if isinstance(op2, str):
+                        op2 = float(op2)
+                else:
+                    return self.err('error_invalid_litteral', '/')
+                result = op2 / op1
+                self.interpreter.work.appendleft(result)
+                return 'nobreak'
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
