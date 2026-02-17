109f4194286548a8f584f4173395dabd6bb2dc4f
fvissault <fv35@yahoo.fr>
2026-02-17 14:11:15 +0100

    Update logic.py

--- packages/logic.py
+++ packages/logic.py
@@ -1,159 +1,209 @@
+import traceback
+
 class Logic:
     
     '''
     Instruction = : Egalité
     '''
     def equal_instr(self):
-        if self.require_stack(2, '=') == None:
-            base = self.dictionary['base']
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            if base != 10:
-                if isinstance(op1, str):
-                    try:
-                        op1 = int(op1, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '=')
-                if isinstance(op2, str):
-                    try:
-                        op2 = int(op2, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '=')
-            if op2 == op1:
-                self.interpreter.work.appendleft(1)
-            else:
-                self.interpreter.work.appendleft(0)
-            return 'nobreak'   
+        try:
+            if self.require_stack(2, '=') == None:
+                base = self.dictionary['base']
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                if base != 10:
+                    if isinstance(op1, str):
+                        try:
+                            op1 = int(op1, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '=')
+                    if isinstance(op2, str):
+                        try:
+                            op2 = int(op2, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '=')
+                if op2 == op1:
+                    self.interpreter.work.appendleft(1)
+                else:
+                    self.interpreter.work.appendleft(0)
+                return 'nobreak'   
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     '''
     Instruction > : Supérieur
     '''
     def sup_instr(self):
-        if self.require_stack(2, '>') == None:
-            base = self.dictionary['base']
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            if base != 10:
-                if isinstance(op1, str):
-                    try:
-                        op1 = int(op1, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '>')
-                if isinstance(op2, str):
-                    try:
-                        op2 = int(op2, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '>')
-            if op2 > op1:
-                self.interpreter.work.appendleft(1)
-            else:
-                self.interpreter.work.appendleft(0)
-            return 'nobreak'   
+        try:
+            if self.require_stack(2, '>') == None:
+                base = self.dictionary['base']
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                if base != 10:
+                    if isinstance(op1, str):
+                        try:
+                            op1 = int(op1, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '>')
+                    if isinstance(op2, str):
+                        try:
+                            op2 = int(op2, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '>')
+                if op2 > op1:
+                    self.interpreter.work.appendleft(1)
+                else:
+                    self.interpreter.work.appendleft(0)
+                return 'nobreak'   
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     '''
     Instruction < : Inférieur
     '''
     def inf_instr(self):
-        if self.require_stack(2, '<') == None:
-            base = self.dictionary['base']
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            if base != 10:
-                if isinstance(op1, str):
-                    try:
-                        op1 = int(op1, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '<')
-                if isinstance(op2, str):
-                    try:
-                        op2 = int(op2, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '<')
-            if op2 < op1:
-                self.interpreter.work.appendleft(1)
-            else:
-                self.interpreter.work.appendleft(0)
-            return 'nobreak'   
+        try:
+            if self.require_stack(2, '<') == None:
+                base = self.dictionary['base']
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                if base != 10:
+                    if isinstance(op1, str):
+                        try:
+                            op1 = int(op1, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '<')
+                    if isinstance(op2, str):
+                        try:
+                            op2 = int(op2, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '<')
+                if op2 < op1:
+                    self.interpreter.work.appendleft(1)
+                else:
+                    self.interpreter.work.appendleft(0)
+                return 'nobreak'   
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     '''
     Instruction >= : Supérieur ou égal
     '''
     def supequal_instr(self):
-        if self.require_stack(2, '>=') == None:
-            base = self.dictionary['base']
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            if base != 10:
-                if isinstance(op1, str):
-                    try:
-                        op1 = int(op1, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '>=')
-                if isinstance(op2, str):
-                    try:
-                        op2 = int(op2, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '>=')
-            if op2 >= op1:
-                self.interpreter.work.appendleft(1)
-            else:
-                self.interpreter.work.appendleft(0)
-            return 'nobreak'   
+        try:
+            if self.require_stack(2, '>=') == None:
+                base = self.dictionary['base']
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                if base != 10:
+                    if isinstance(op1, str):
+                        try:
+                            op1 = int(op1, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '>=')
+                    if isinstance(op2, str):
+                        try:
+                            op2 = int(op2, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '>=')
+                if op2 >= op1:
+                    self.interpreter.work.appendleft(1)
+                else:
+                    self.interpreter.work.appendleft(0)
+                return 'nobreak'   
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     '''
     Instruction <= : Inférieur ou égal
     '''
     def infequal_instr(self):
-        if self.require_stack(2, '<=') == None:
-            base = self.dictionary['base']
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            if base != 10:
-                if isinstance(op1, str):
-                    try:
-                        op1 = int(op1, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '<=')
-                if isinstance(op2, str):
-                    try:
-                        op2 = int(op2, base)
-                    except(ValueError):
-                        return self.err('error_invalid_litteral', '<=')
-            if op2 <= op1:
-                self.interpreter.work.appendleft(1)
-            else:
-                self.interpreter.work.appendleft(0)
-            return 'nobreak'   
+        try:
+            if self.require_stack(2, '<=') == None:
+                base = self.dictionary['base']
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                if base != 10:
+                    if isinstance(op1, str):
+                        try:
+                            op1 = int(op1, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '<=')
+                    if isinstance(op2, str):
+                        try:
+                            op2 = int(op2, base)
+                        except(ValueError):
+                            return self.err('error_invalid_litteral', '<=')
+                if op2 <= op1:
+                    self.interpreter.work.appendleft(1)
+                else:
+                    self.interpreter.work.appendleft(0)
+                return 'nobreak'   
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     '''
     Instruction and : et logique
     '''
     def and_instr(self):
-        if self.require_stack(2, 'and') == None:
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            result = op1 and op2
-            self.interpreter.work.appendleft(result)
-            return 'nobreak'
+        try:
+            if self.require_stack(2, 'and') == None:
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                result = op1 and op2
+                self.interpreter.work.appendleft(result)
+                return 'nobreak'
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     '''
     Instruction ou : ou logique
     '''
     def or_instr(self):
-        if self.require_stack(2, 'or') == None:
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            result = op1 or op2
-            self.interpreter.work.appendleft(result)
-            return 'nobreak'
+        try:
+            if self.require_stack(2, 'or') == None:
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                result = op1 or op2
+                self.interpreter.work.appendleft(result)
+                return 'nobreak'
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
 
     '''
     Instruction xor : ou exclusif logique
     '''
     def xor_instr(self):
-        if self.require_stack(2, 'xor') == None:
-            op1 = self.pop_work()
-            op2 = self.pop_work()
-            result = op1 ^ op2
-            self.interpreter.work.appendleft(result)
-            return 'nobreak'
+        try:
+            if self.require_stack(2, 'xor') == None:
+                op1 = self.pop_work()
+                op2 = self.pop_work()
+                result = op1 ^ op2
+                self.interpreter.work.appendleft(result)
+                return 'nobreak'
+        except Exception as e:
+            tb = traceback.TracebackException.from_exception(e)
+            print("".join(tb.format()))
+            self.logerr("".join(tb.format()))        
+            return "break"
