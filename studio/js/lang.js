f833e62847a76b129b1ad6c26680d6f4a2b507a2
fvissault <fv35@yahoo.fr>
2026-03-30 10:09:51 +0200

    Create lang.js

---
+++ studio/js/lang.js
@@ -0,0 +1,48 @@
+const supportedLangs = ["fr", "en"]
+
+function detectBrowserLang() {
+    const lang = navigator.language
+    return lang.split("-")[0]
+}
+
+function getInitialLanguage() {
+    // 1. choix utilisateur
+    const saved = localStorage.getItem("lang")
+    if (saved && supportedLangs.includes(saved)) {
+        return saved
+    }
+
+    // 2. navigateur
+    const browserLang = detectBrowserLang()
+    if (supportedLangs.includes(browserLang)) {
+        return browserLang
+    }
+
+    // 3. fallback
+    return "en"
+}
+
+let currentLang = getInitialLanguage()
+
+function setLanguage(lang) {
+    currentLang = lang
+    localStorage.setItem("lang", lang)
+    renderUI()
+}
+
+function t(key) {
+    return translations[currentLang]?.[key]
+        || translations["en"]?.[key]
+        || key
+}
+
+function renderUI() {
+    document.querySelectorAll("[data-i18n]").forEach(el => {
+        const [type, key] = el.dataset.i18n.split(":")
+        if (type === "placeholder") {
+            el.placeholder = t(key)
+        } else {
+            el.innerText = t(key)
+        }
+    })
+}
