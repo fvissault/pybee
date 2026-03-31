9dd17bf87f071886189e4150d9b5242cc3c0e800
fvissault <fv35@yahoo.fr>
2026-03-31 16:38:19 +0200

    Update mail.py

--- studio/api/mail.py
+++ studio/api/mail.py
@@ -5,64 +5,73 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 import traceback
 from utils import *
-import predefined_mails
-import config_mails
+from predefined_mails import predefined_mails
+from config_mails import config_mails
 import html
 
-
-# data = { "lang" : "fr|en|...", "predname" : "passlost | signin", "email" : "emailtosend", "toreplace" : { "name" : "fred", "link" : "http://www.yahoo.fr" }}
-data = get_post_data()
-emailtosend = data.get("email", "")
-if not is_valid_email(emailtosend):
-    json_response({"status": "invalid email"})
-else:
-    toreplace = data.get("toreplace", {})
-    lang = data.get("lang", "en")
-    predname = data.get("predname")
-    if lang not in predefined_mails or predname not in predefined_mails.get(lang, {}):
-        json_response({"status": "invalid template"})
+try:
+    data = get_post_data()
+    emailtosend = data.get("email", "")
+    if not is_valid_email(emailtosend):
+        json_response({"status": "invalid email"})
     else:
-        predemail = predefined_mails[lang][predname].copy()
-        predemail['subject'] = predemail['subject'].format(**toreplace)
-        safe_replace = {k: html.escape(str(v)) for k, v in toreplace.items()}
-        predemail['body'] = predemail['body'].format(**safe_replace)
-        predemail['text'] = predemail['text'].format(**toreplace)
+        toreplace = data.get("toreplace", {})
+        if isinstance(toreplace, str):
+            try:
+                toreplace = json.loads(toreplace)
+            except:
+                toreplace = {}
+        lang = data.get("lang", "en")
+        predname = data.get("predname")
+        if lang not in predefined_mails or predname not in predefined_mails.get(lang, {}):
+            json_response({"status": "invalid template"})
+        else:
+            predemail = predefined_mails[lang][predname]
+            try:
+                predemail['subject'] = predemail['subject'].format(**toreplace)
+                predemail['text'] = predemail['text'].format(**toreplace)
+            except KeyError as e:
+                json_response({"status": "template error", "missing": str(e)})
+            safe_replace = {k: html.escape(str(v)) for k, v in toreplace.items()}
+            predemail['body'] = predemail['body'].format(**safe_replace)
 
-        message = MIMEMultipart("alternative")
-        message["Subject"] = predemail["subject"]
-        message["From"] = config_mails["username"]
-        message["To"] = emailtosend
+            message = MIMEMultipart("alternative")
+            message["Subject"] = predemail["subject"]
+            message["From"] = config_mails["username"]
+            message["To"] = emailtosend
 
-        # Create the plain-text and HTML version of your message
-        text = predemail["text"]
-        html = predemail["body"]
+            # Create the plain-text and HTML version of your message
+            text = predemail["text"]
+            html_body = predemail["body"]
 
-        # Turn these into plain/html MIMEText objects
-        part1 = MIMEText(text, "plain", "utf-8")
-        part2 = MIMEText(html, "html", "utf-8")
+            # Turn these into plain/html MIMEText objects
+            part1 = MIMEText(text, "plain", "utf-8")
+            part2 = MIMEText(html_body, "html", "utf-8")
 
-        # Add HTML/plain-text parts to MIMEMultipart message
-        # The email client will try to render the last part first
-        message.attach(part1)
-        message.attach(part2)
+            # Add HTML/plain-text parts to MIMEMultipart message
+            # The email client will try to render the last part first
+            message.attach(part1)
+            message.attach(part2)
 
-        if "attachments" in predemail:
-            for filepath in predemail["attachments"]:
-                if os.path.exists(filepath):
-                    with open(filepath, "rb") as f:
-                        part = MIMEApplication(f.read(), Name=os.path.basename(filepath))
-                        part['Content-Disposition'] = f'attachment; filename="{os.path.basename(filepath)}"'
-                        message.attach(part)
+            if "attachments" in predemail:
+                for filepath in predemail["attachments"]:
+                    if os.path.exists(filepath):
+                        with open(filepath, "rb") as f:
+                            part = MIMEApplication(f.read(), Name=os.path.basename(filepath))
+                            part['Content-Disposition'] = f'attachment; filename="{os.path.basename(filepath)}"'
+                            message.attach(part)
 
-        try:
-            context = ssl.create_default_context()
-            with smtplib.SMTP_SSL(config_mails["host"], config_mails["port"], context=context) as server:
-                server.login(config_mails["username"], config_mails["password"])
-                server.sendmail(config_mails["username"], emailtosend, message.as_string())
-                json_response({"status": "sent"})
-        except smtplib.SMTPAuthenticationError:
-            json_response({"status": "Send mail error : auth failed"})
-        except Exception as e:
-            traceback.print_exc()
-            json_response({"status": "error", "message": str(e)})
-            
+            try:
+                context = ssl.create_default_context()
+                with smtplib.SMTP_SSL(config_mails["host"], config_mails["port"], context=context) as server:
+                    server.login(config_mails["username"], config_mails["password"])
+                    server.sendmail(config_mails["username"], emailtosend, message.as_string())
+                    json_response({"status": "sent"})
+            except smtplib.SMTPAuthenticationError:
+                json_response({"status": "Send mail error : auth failed"})
+            except Exception as e:
+                traceback.print_exc()
+                json_response({"status": "error", "message": str(e)})
+except Exception as e:
+    traceback.print_exc()
+    json_response({"status": "fatal error", "message": str(e)})
