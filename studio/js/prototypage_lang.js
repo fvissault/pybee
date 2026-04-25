c772ac5c12a8422821e8b35677c84f137c4aa691
fvissault <fv35@yahoo.fr>
2026-04-25 10:59:21 +0200

    Create prototypage_lang.js

---
+++ studio/js/prototypage_lang.js
@@ -0,0 +1,46 @@
+const translations = {
+    fr : { 
+        title: "Beetle Studio Prototype",
+        proto_header : "Beetle Studio - Prototypage", 
+        newworkspace : "Nouvel espace de travail", 
+        save : "Sauvegarder", 
+        explorer: "Explorateur",
+        palette: "Palette d'outils",
+        layout: "Mise en page",
+        containers: "Containers",
+        simpleobjs: "Objets simples",
+        layout1z: "Mise en page 1 zone",
+        layout2z: "Mise en page 2 zones",
+        layout3z: "Mise en page 3 zones",
+        layout4z: "Mise en page 4 zones",
+        layout5z: "Mise en page 5 zones",
+        block: "Division",
+        text: "Texte brut",
+        span: "Zone de texte",
+        label: "Etiquette",
+        textfield: "Champ de texte",
+        form: "Formulaire",
+        img: "Image",
+        button: "Bouton",
+        anchor: "Ancre",
+        li: "Item de liste",
+        ul: "Liste non ordonnée",
+        ol: "Liste ordonnée",
+        workspace: "Espace de travail",
+        trash: "🗑 Glisser ici pour supprimer",
+        param: "Paramétrage",
+        css: "Styling",
+        internflow: "Flux internes",
+        model: "Modélisation"
+    } , 
+    en : { 
+    } ,
+    it : {
+    },
+    de : {
+    } ,
+    es : {
+    }
+}
+
+renderUI()
