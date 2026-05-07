11834357bb4ffdedaa0c0ddfdbdaf4e02ec10d9f
fvissault <fv35@yahoo.fr>
2026-05-07 14:48:00 +0200

    Update prototypage_render.js

--- studio/js/prototypage_render.js
+++ studio/js/prototypage_render.js
@@ -1,3 +1,12 @@
+function createNewButton(textcontent, title) {
+    const btn = document.createElement("button")
+    btn.textContent = textcontent
+    btn.title = title
+    btn.style.fontWeight = "bold"
+    btn.className = "btn btn-secondary"
+    return btn
+}
+
 function render(){
     workspaceEl.innerHTML=""
     if(!workspaceRoot)
@@ -36,22 +45,15 @@ function renderZoneLayout(zone){
     zoneEl.dataset.nodeId=zone.id
     const label=document.createElement("span")
     label.textContent=zone.id
-    const cssBtn=document.createElement("button")
-    cssBtn.textContent="::"
-    cssBtn.title = "CSS"
-    cssBtn.style.fontWeight = "bold"
-    cssBtn.className = "btn btn-secondary"
+    const cssBtn = createNewButton("::", "CSS")
     cssBtn.style.marginRight="6px"
     cssBtn.onclick=(e)=>{
         e.stopPropagation()
         openDialog(zone, 'css')
     }
-    const htmlBtn=document.createElement("button")
-    htmlBtn.textContent="⚙"
-    htmlBtn.title = "Paramètres"
+    const htmlBtn = createNewButton("⚙", "Paramètres")
     htmlBtn.style.fontSize = "12px"
-    htmlBtn.className = "btn btn-secondary"
-    htmlBtn.style.marginRight="6px"
+    htmlBtn.style.marginRight = "6px"
     htmlBtn.onclick=(e)=>{
         e.stopPropagation()
         openDialog(zone, 'html')
@@ -71,21 +73,14 @@ function renderLayout(layout) {
     el.dataset.nodeId = layout.id
     const label=document.createElement("span")
     label.textContent=layout.id
-    const cssBtn=document.createElement("button")
-    cssBtn.textContent="::"
-    cssBtn.title = "CSS"
-    cssBtn.style.fontWeight = "bold"
-    cssBtn.className = "btn btn-secondary"
+    const cssBtn = createNewButton("::", "CSS")
     cssBtn.style.marginRight="6px"
     cssBtn.onclick=(e) => {
         e.stopPropagation()
         openDialog(workspaceRoot, "lcss")
     }
-    const htmlBtn=document.createElement("button")
-    htmlBtn.textContent="⚙"
-    htmlBtn.title = "Paramètres"
+    const htmlBtn = createNewButton("⚙", "Paramètres")
     htmlBtn.style.fontSize = "12px"
-    htmlBtn.className = "btn btn-secondary"
     htmlBtn.style.marginRight="6px"
     htmlBtn.onclick=(e) => {
         e.stopPropagation()
@@ -101,11 +96,8 @@ function renderLayout(layout) {
 function renderWidget(widget){
     const el=document.createElement("div")
     el.className="widget"
-    const htmlBtn=document.createElement("button")
-    htmlBtn.textContent="⚙"
-    htmlBtn.title = "Paramètres"
+    const htmlBtn = createNewButton("⚙", "Paramètres")
     htmlBtn.style.fontSize = "12px"
-    htmlBtn.className = "btn btn-secondary"
     htmlBtn.style.marginRight = "6px"
     htmlBtn.onclick=(e) => {
         e.stopPropagation()
@@ -114,22 +106,15 @@ function renderWidget(widget){
     let cssBtn = null
     let eventsBtn = null
     if (widget.widgetType != "Text" && widget.widgetType != "Form") {
-        cssBtn=document.createElement("button")
-        cssBtn.textContent="::"
-        cssBtn.title = "CSS"
-        cssBtn.style.fontWeight = "bold"
-        cssBtn.className = "btn btn-secondary"
+        cssBtn = createNewButton("::", "CSS")
         cssBtn.style.marginRight="6px"
         cssBtn.onclick=(e) => {
             e.stopPropagation()
             openDialog(widget, "css")
         }   
         if (widget.widgetType != "Anchor") {
-            eventsBtn=document.createElement("button")
-            eventsBtn.textContent="e"
-            eventsBtn.title = "Paramètres"
+            eventsBtn = createNewButton("e", "Evènements")
             eventsBtn.style.fontSize = "14px"
-            eventsBtn.className = "btn btn-secondary"
             eventsBtn.style.marginRight = "6px"
             eventsBtn.onclick=(e) => {
                 e.stopPropagation()
@@ -153,6 +138,37 @@ function renderWidget(widget){
     el.draggable = true
     if (widget.container) el.appendChild(renderZone(widget.children[0]))
     return el
+}
+
+function createItemContainer() {
+    const element = document.createElement("div")
+    element.className = "tree-file"
+    element.style.display = "flex"
+    element.style.alignItems = "center"
+    return element
+}
+
+function createItemLabel(textcontent) {
+    const label = document.createElement("span")
+    label.textContent = textcontent
+    label.style.cursor = "pointer"
+    return label
+}
+
+function createDelButton(title) {
+    const del = document.createElement("button")
+    del.className = "btn btn-secondary btn-delete"
+    del.title = title
+    del.style.padding = "4px 5px 0px 7px"
+    del.innerHTML = `<svg class="icon_explorer" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
+    <line x1="9" y1="5" x2="15" y2="5"/>
+    <line x1="5" y1="6" x2="19" y2="6"/>
+    <rect x="7" y="6" width="10" height="12" rx="2"/>
+    <line x1="11" y1="10" x2="11" y2="15"/>
+    <line x1="14" y1="10" x2="14" y2="15"/>
+</svg>`
+    del.style.marginLeft = "auto"
+    return del    
 }
 
 function renderProjectFiles() {
@@ -196,28 +212,12 @@ function renderProjectFiles() {
 
     if (!pages.error) {
         pages.forEach(f => {
-            const el = document.createElement("div")
-            el.className = "tree-file"
-            el.style.display = "flex"
-            el.style.alignItems = "center"
-            const label = document.createElement("span")
-            label.textContent = "📄 " + f.pagename
-            label.style.cursor = "pointer"
+            const el = createItemContainer()
+            const label = createItemLabel("📄 " + f.pagename)
             label.addEventListener("click", () => {
                 loadBST(f.id)
             })
-            const del = document.createElement("button")
-            del.className = "btn btn-secondary"
-            del.title = "Supprimer la page"
-            del.style.padding = "4px 6px 0px 7px"
-            del.innerHTML = `<svg class="icon_explorer" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
-    <line x1="9" y1="5" x2="15" y2="5"/>
-    <line x1="5" y1="6" x2="19" y2="6"/>
-    <rect x="7" y="6" width="10" height="12" rx="2"/>
-    <line x1="11" y1="10" x2="11" y2="15"/>
-    <line x1="14" y1="10" x2="14" y2="15"/>
-</svg>`
-            del.style.marginLeft = "auto"
+            const del = createDelButton("Supprimer la page")
             del.addEventListener("click", (e) => {
                 e.stopPropagation()
                 deleteBST(f.id)
@@ -234,13 +234,8 @@ function renderProjectFiles() {
     if (typeof components !== "undefined" && !components.error) {
         components.forEach(c => {
             let popups = JSON.parse(c.popups)
-            const el = document.createElement("div")
-            el.className = "tree-file"
-            el.style.display = "flex"
-            el.style.alignItems = "center"
-            const label = document.createElement("span")
-            label.textContent = `🧩 ${c.name} (${popups.length})`
-            label.style.cursor = "pointer"
+            const el = createItemContainer()
+            const label = createItemLabel(`🧩 ${c.name} (${popups.length})`)
             label.addEventListener("click", () => {
                 loadComponent(c.id) // à adapter à ton code
             })
@@ -265,33 +260,18 @@ function renderProjectFiles() {
             el.appendChild(newPopup)
             componentsContainer.appendChild(el)
             // 🔸 POPUPS (niveau inférieur)
-            if (popups && popups.length > 0) {
+            let comppops = JSON.parse(c.popups)
+            if (comppops && comppops.length > 0) {
                 const popupContainer = document.createElement("div")
                 popupContainer.style.marginLeft = "16px"
-                popups.forEach((p, index) => {
-                    const popupEl = document.createElement("div")
-                    popupEl.className = "tree-file"
-                    popupEl.style.display = "flex"
-                    popupEl.style.alignItems = "center"
-                    const popupLabel = document.createElement("span")
-                    popupLabel.textContent = `⚙️ admin ${index + 1} ${p.props.name?p.props.name:""}`
-                    popupLabel.style.cursor = "pointer"
+                comppops.forEach((p, index) => {
+                    const popupEl = createItemContainer()
+                    const popupLabel = createItemLabel(`⚙️ admin ${index + 1} ${p.props.name?p.props.name:""}`)
                     popupLabel.style.opacity = "0.8" // léger différenciateur
                     popupLabel.addEventListener("click", () => {
                         loadPopup(c.id, index)
                     })
-                    const del = document.createElement("button")
-                    del.className = "btn btn-secondary"
-                    del.title = "Supprimer la popup"
-                    del.style.padding = "4px 6px 0px 7px"
-                    del.innerHTML = `<svg class="icon_explorer" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round">
-            <line x1="9" y1="5" x2="15" y2="5"/>
-            <line x1="5" y1="6" x2="19" y2="6"/>
-            <rect x="7" y="6" width="10" height="12" rx="2"/>
-            <line x1="11" y1="10" x2="11" y2="15"/>
-            <line x1="14" y1="10" x2="14" y2="15"/>
-        </svg>`
-                    del.style.marginLeft = "auto"
+                    const del = createDelButton("Supprimer la popup")
                     del.addEventListener("click", (e) => {
                         e.stopPropagation()
                         deletePopup(c.id, index)
