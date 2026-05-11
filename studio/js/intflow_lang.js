9aa3deb703f4e9fef351521ac174a61d3ff0c6c9
fvissault <fv35@yahoo.fr>
2026-05-11 09:16:34 +0200

    Create intflow_lang.js

---
+++ studio/js/intflow_lang.js
@@ -0,0 +1,18 @@
+const translations = {
+    fr : { 
+        intflow_header: "Beetle Studio - Editeur de flux interne",
+        save: "Sauvegarder",
+        close: "Fermer",
+        trash: "🗑 Glisser ici pour supprimer"
+    } , 
+    en : { 
+    },
+    it : { 
+    },
+    de : { 
+    },
+    es : { 
+    }
+}
+
+renderUI()
