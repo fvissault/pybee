c2df5baa8ed39326f3a170aed9c32e2e0d2e0bed
fvissault <fv35@yahoo.fr>
2026-08-04 18:05:54 +0200

    Update intflow_buttons.js

--- studio/js/intflow_buttons.js
+++ studio/js/intflow_buttons.js
@@ -3,6 +3,7 @@
  *==================================================================================*/
 function save() {
     try {
+        const session = window.opener.getSession()
         fetch("/pybee/studio/api/jsfiles.py", {
             method: "POST",
             credentials: "include",
@@ -25,7 +26,7 @@ function save() {
 
 function closefct() {
     if (tosave) {
-        check = confirm("Etes-vous sur de fermer IntFlow sans sauvegarder votre travail ?")
+        check = confirm("Etes-vous sur de fermer InternFlow sans sauvegarder votre travail ?")
         if (check) window.close()
     } else {
         window.close()
@@ -34,6 +35,7 @@ function closefct() {
 
 function generatejsfile() {
     try {
+        const session = window.opener.getSession()
         fetch("/pybee/studio/api/file_access_api.py?action=save_js_file&entity=" + projectname, {
             method: "POST",
             credentials: "include",
@@ -56,42 +58,3 @@ function generatejsfile() {
     }
 
 }
-
-/*==================================================================================
- * UI SAVE BUTTON
- *==================================================================================*/
-/*const saveBtn = document.createElement("button")
-saveBtn.innerText = "Save"
-saveBtn.id = "savebtn"
-saveBtn.style.position = "fixed"
-saveBtn.style.bottom = "10px"
-saveBtn.style.right = "60px"
-saveBtn.onclick = save
-document.body.appendChild(saveBtn)
-
-const closeBtn = document.createElement("button")
-closeBtn.innerText = "Close"
-closeBtn.style.position = "fixed"
-closeBtn.style.bottom = "10px"
-closeBtn.style.right = "10px"
-closeBtn.onclick = closefct
-document.body.appendChild(closeBtn)
-*/
-/*==================================================================================
- * UI TRASH BUTTON
- *==================================================================================*/
-/*const trash = document.createElement("div")
-trash.innerText = "🗑️"
-trash.style.alignItems = "center"
-trash.style.display = "flex"
-trash.style.justifyContent = "center"
-trash.style.position = "fixed"
-trash.style.top = "10px"
-trash.style.right = "10px"
-trash.style.width = "50px"
-trash.style.height = "200px"
-trash.style.backgroundColor = "#eee"
-trash.style.border = "1px solid gray"
-trash.style.borderRadius = "5px"
-document.body.appendChild(trash)
-*/
