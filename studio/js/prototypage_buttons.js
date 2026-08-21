0e5c83e54d1d9a9b98d3800fddd3e12473beee2f
fvissault <fv35@yahoo.fr>
2026-08-21 13:07:27 +0200

    Update prototypage_buttons.js

--- studio/js/prototypage_buttons.js
+++ studio/js/prototypage_buttons.js
@@ -1,38 +1,3 @@
-/*************************************************************************************************************
- * Reset de page et de composant
- *************************************************************************************************************/
-function resetComponent() {
-    workspaceRoot = {
-        id:generateId("Composant"),
-        type:"container",
-        props:{},
-        css:{},
-        js:{},
-        events:{},
-        children:[],
-        popups:[]
-    }
-    workspaceEl.innerHTML = ""
-    currentPage = null
-    currentComponent = "new-component"
-    currentPopup = null
-    perspective = "component"
-    document.getElementById("workspace_content").innerText = "Création d'un composant"
-}
-
-document.getElementById("newComposantBtn").addEventListener("click", ()=> {
-    if (tosave) {
-        let check = confirm("Voulez-vous enregistrer votre travail?")
-        if (!check) {
-            resetComponent()
-            tosave = false
-            document.getElementById("savebtn").className = ""
-        }
-    } else {
-        resetComponent()
-    }
-})
-
 /*function resetPage() {
     workspaceRoot = {
         id:generateId("Container"),
