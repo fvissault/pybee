88f1f8facd58b26090d15d258ef9bd31821ce2be
fvissault <fv35@yahoo.fr>
2026-08-21 13:06:53 +0200

    Update intflow_buttons.js

--- studio/js/intflow_buttons.js
+++ studio/js/intflow_buttons.js
@@ -29,15 +29,21 @@ function save() {
 function closefct() {
     if (tosave) {
         check = confirm("Etes-vous sur de fermer InternFlow sans sauvegarder votre travail ?")
-        if (check) window.close()
+        if (check) {
+            if (preview) preview.close()
+            window.close()
+        }
     } else {
+        if (preview) preview.close()
         window.close()
     }
 }
 
+let preview = null
 function generatejsfile() {
     try {
         const session = window.opener.getSession()
+        const generatedString = generate(tree)
         fetch("/pybee/studio/api/file_access_api.py?action=save_js_file&entity=" + projectname, {
             method: "POST",
             credentials: "include",
@@ -45,14 +51,20 @@ function generatejsfile() {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                file_content: generate(tree),
+                file_content: generatedString,
                 file_name: pagename
             })
         })
         .then(r => r.json())
         .then(data => {
             if (data.status === "ok") {
-                alert("Votre fichier js a bien été généré")
+                alert("Votre fichier js a bien été généré. Vous allez le visualiser...")
+                if (!preview) {
+                    preview = window.open("", "_blank", "popup=yes,width=800,height=600")
+                }
+                preview.document.title = `Prévisualisation de ${pagename}.js`;
+                preview.document.body.innerHTML = `<pre style="white-space: pre-wrap; font-family: monospace;">${generatedString}</pre>`;
+                preview.focus()
             }
         });
     } catch(e) {
